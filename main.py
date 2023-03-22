c1b0960297cb2d8420f243551f3ea7f61d55ed52
yut_fut <seregey.golubev@mail.ru>
2023-03-22 11:07:42 +0300

    edite init_db.sql

--- main.py
+++ main.py
@@ -99,12 +99,8 @@ with open('new_file3.csv') as f:
 for item in data:
     if item == data[0]:
         continue
-    # print(item[2])
-    # print(print_hi(item[2]))
-    # if len(item[2]) == 0:
-    #     item[2] == ""
     cursor.execute(
-    '''INSERT INTO task (name, description, public_tests, difficulty, cf_contest_id, cf_index, cf_points, cf_rating, cf_tags, time_limit, memory_limit_bytes, link, task_ru, input, output, note) VALUES (E'{0}', E'{1}', ARRAY{2}, ARRAY'{3}', ARRAY'{4}', '{5}', '{6}', '{7}', '{8}', '{9}', '{10}', '{11}', '{12}', '{13}', E'{14}', E'{15}', E'{16}', E'{17}'),\n'''.format(
+    '''INSERT INTO task (name, description, public_tests, private_tests, generated_tests, difficulty, cf_contest_id, cf_index, cf_points, cf_rating, cf_tags, time_limit, memory_limit_bytes, link, task_ru, input, output, note) VALUES (E'{0}', E'{1}', ARRAY{2}, ARRAY{3}, ARRAY{4}, '{5}', '{6}', '{7}', '{8}', '{9}', '{10}', '{11}', '{12}', '{13}', E'{14}', E'{15}', E'{16}', E'{17}');'''.format(
         item[0].replace("'", r"\'"), item[1].replace('\\', r"\\").replace("'", r"\'"), print_hi(item[2]), print_hi(item[3]), print_hi(item[4]), item[5], item[6], item[7],
         item[8], item[9], str(item[10].replace("'", "")), item[11], item[12], item[13], item[14].replace('\\', r"\\").replace("'", r"\'"), item[15].replace('\\', r"\\").replace("'", r"\'"), item[16].replace('\\', r"\\").replace("'", r"\'"), item[17].replace('\\', r"\\").replace("'", r"\'")))
     conn.commit()
